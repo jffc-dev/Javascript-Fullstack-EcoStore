062f0d499b9637b667a540f735e28dca4d9ab286
Javier Flores <jffc.dev@gmail.com>
2023-02-14 18:52:44 -0500

    fix(Api): static and types fixed

--- EcoStore-Api/index.ts
+++ EcoStore-Api/index.ts
@@ -1,13 +1,16 @@
-import express, { Express, Request, Response } from 'express';
+import express, { type Express, type Request, type Response } from 'express'
 import 'dotenv/config'
+import path from 'path'
 
-const app: Express = express();
-const port = process.env.PORT;
+const app: Express = express()
+const dirname = path.resolve()
+const port: string = (process.env.PORT === null || process.env.PORT === undefined) ? '8000' : process.env.PORT
+app.use(express.static(path.join(dirname, '/public/dist')))
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
+  res.send('Express + TypeScript Server')
+})
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+  console.log(`[server]: Server is running at http://localhost:${port}`)
+})
